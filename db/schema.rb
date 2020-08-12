5ef72290febe94cc43233ceff598031331806a9d
samdaly97 <sam.daly97@gmail.com>
2020-08-12 15:07:02 +0000

    final commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_193446) do
+ActiveRecord::Schema.define(version: 2020_08_11_003151) do
 
   create_table "answers", force: :cascade do |t|
     t.string "reply"
@@ -28,14 +28,6 @@ ActiveRecord::Schema.define(version: 2020_08_11_193446) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_problems_on_user_id"
-  end
-
-  create_table "purchases", force: :cascade do |t|
-    t.boolean "paid", default: false
-    t.string "token"
-    t.integer "price"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "sessions", force: :cascade do |t|
